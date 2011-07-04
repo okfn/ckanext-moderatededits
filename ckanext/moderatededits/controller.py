f0cd56a496d2e6f4abfefc61483eeb2b07c00dc4
John Glover <j@johnglover.net>
2011-07-04 16:26:30 +0100

    Add list of moderated packages and packages with pending changes to account page

--- ckanext/moderatededits/controller.py
+++ ckanext/moderatededits/controller.py
@@ -4,11 +4,17 @@ CKAN Moderated Edits Extension
 from logging import getLogger
 log = getLogger(__name__)
 
-from pylons.i18n import _
-from pylons.decorators import jsonify
-from pylons import request, tmpl_context as c
-from ckan.lib.base import BaseController, response, render, abort
-from ckanext.moderatededits import model
+from ckan.lib.base import BaseController
+from ckan import model
+
+def get_moderated_packages(context):
+    """
+    Return a list of all packages moderated by the user in the given context.
+    """
+    session = model.meta.Session()
+    q = model.Session.query(model.PackageRole)
+    q = q.filter_by(user=model.User.get(context.user), role=model.Role.ADMIN)
+    return [model.Package.get(p.package_id) for p in q]
 
 class ModeratedEditsController(BaseController):
     """
--- ckanext/moderatededits/plugin.py
+++ ckanext/moderatededits/plugin.py
@@ -15,18 +15,17 @@ log = getLogger(__name__)
 from genshi.input import HTML
 from genshi.filters import Transformer
 from pylons import request, tmpl_context as c
-from webob import Request
 from ckan.lib.base import h
 from ckan.plugins import SingletonPlugin, implements
 from ckan.plugins.interfaces import (IConfigurable, IRoutes, 
                                      IGenshiStreamFilter, IConfigurer)
 
-from ckanext.moderatededits import model
 from ckanext.moderatededits import controller
 from ckanext.moderatededits import html
 
 class ModeratedEditsPlugin(SingletonPlugin):
     """
+    CKAN Moderated Edits Plugin
     """
     implements(IConfigurable)
     implements(IConfigurer, inherit=True)
@@ -102,4 +101,38 @@ class ModeratedEditsPlugin(SingletonPlugin):
             # add revision list widget
             stream = stream | Transformer('body//div[@id="primary"]/ul')\
                 .append(HTML(html.REVISION_LIST_CODE))
+
+        # if this is the read action of a user page, show packages being followed
+        elif(routes.get('controller') == 'user' and
+             routes.get('action') == 'read' and 
+             c.user):
+            moderated_packages = controller.get_moderated_packages(c)
+            mod_html = '<div class="moderation"><h3>Package Moderation</h3><ul>'
+            mod_html += '<li><strong>Number of packages moderated:</strong> ' +\
+                str(len(moderated_packages))
+            mod_html += '</li>'
+            if moderated_packages:
+                mod_html += '<li><strong>Moderated packages:</strong> '
+                for i, pkg in enumerate(moderated_packages):
+                    mod_html += str(h.link_to(pkg.name, h.url_for(
+                        controller='package', action='read', id=pkg.name
+                    ))) 
+                    if i < len(moderated_packages) - 1:
+                        mod_html += ', '
+                mod_html += '</li>'
+                pending = [pkg for pkg in moderated_packages if pkg.state == 'pending']
+                mod_html += '<li id="num-pending"><strong>Number of moderated packages ' +\
+                    'with pending changes:</strong> '
+                mod_html += str(len(pending)) + '</li>'
+                if pending:
+                    mod_html += '<li><strong>Packages with pending changes:</strong> '
+                    for n, p in enumerate(pending):
+                        mod_html += str(h.link_to(p.name, h.url_for(
+                            controller='package', action='edit', id=p.name
+                        ))) 
+                        if n < len(pending) - 1:
+                            mod_html += ', '
+            mod_html += '</ul></div>'
+            stream = stream | Transformer('body//div[@class="activity"]//ul')\
+                .after(HTML(mod_html))
         return stream
